640d48fa3d1972d1eb6e31255a56141d1a61d126
Shashank Kumar Dahiya <63150026+Sudeep152@users.noreply.github.com>
2022-11-05 01:43:32 +0530

    Day1

--- src/Arrays/SetMatrixZero.java
+++ src/Arrays/SetMatrixZero.java
@@ -7,13 +7,12 @@ public class SetMatrixZero {
     public static void main(String[] args) {
 
         int[][] arr = {
-                {1, 1, 1},
-                {1, 0, 1},
-                {1, 1, 1}
+                {0,1,2,0},{3,4,5,2},{1,3,1,5}
         };
         //setZeroesNaive(arr);
 
-        EfficientApproach(arr);
+        //EfficientApproach(arr);
+        Best_Solution(arr);
 
     }
 
@@ -96,4 +95,43 @@ public class SetMatrixZero {
             System.out.println();
         }
     }
+
+    static void Best_Solution(int[][] matrix) {
+
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int col0 = 1;
+
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < col; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+
+            }
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
 }
